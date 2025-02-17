d75d4d12f2cde1f189ee5d95ed57edae78ebe569
Andres Villalba G. <66088651+andresvg8@users.noreply.github.com>
2025-02-16 19:15:02 -0500

    Created class to use a URL from OpenAPI as parameter for a RestClient object to send and retrieve answers with ChatGPT

---
+++ src/main/java/com/andresvg/simple_chatgpt/config/OpenAPIConfiguration.java
@@ -0,0 +1,21 @@
+package com.andresvg.simple_chatgpt.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class OpenAPIConfiguration
+{
+    @Value("${openapi.api.key}")
+    private String apiUrl;
+
+    @Bean
+    public RestClient restClient()
+    {
+        return RestClient.builder()
+                .baseUrl(apiUrl)
+                .build();
+    }
+}
